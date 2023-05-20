c2b474ce57b86d791b1f91b668b0ba49ab080f7c
manutonui <tktonui@outlook.com>
2023-05-20 10:37:24 +0300

    edit proxy ports in setupProxy.js

--- auth/src/setupProxy.js
+++ auth/src/setupProxy.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     console.log("Proxying request...")
     app.use('/managers', createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050',
         changeOrigin: true
     }));
 
     app.use('/matches', createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050',
         changeOrigin: true
     }));
 };
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     console.log("Proxying request...")
     app.use('/matches', createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050',
         changeOrigin: true
     }));
 };
