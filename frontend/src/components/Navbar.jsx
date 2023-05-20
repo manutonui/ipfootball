91e4d5349c054353244f6870691e9d7c1ec6552f
manutonui <tktonui@outlook.com>
2023-05-20 10:32:01 +0300

    minor change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-sm navbar-dark">
                 <div className="container">
-                    <Link className="navbar-brand" to="/"><h1>ipfootball</h1></Link>
+                    <Link className="navbar-brand" to="/"><h3>ipfootball</h3></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
