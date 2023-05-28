5ebdc50638f1298e6f415630d7794ef8b4956c94
manutonui <tktonui@outlook.com>
2023-05-28 20:35:10 +0300

    added manager feature to track match posters

--- backend/controllers/matchesController.js
+++ backend/controllers/matchesController.js
@@ -1,4 +1,5 @@
 const Match = require('../models/Match')
+const Manager = require('../models/Manager')
 
 const getMatches = async (req, res) => {
     const {date} = req.params // if provided
@@ -18,9 +19,10 @@ const postMatch = async (req, res) => {
     const {home, away, date, tip, odds} = req.body
     if (!home || !away || !date || !tip || !odds) return res.status(400).json({ error: "All fields are required." })
     const fixture = home+' - '+away
+    let author = await Manager.findOne({_id:req.manager_id})
     
     try {
-        const match = await Match.create({fixture, date, tip, odds})
+        const match = await Match.create({fixture, date, tip, odds, author:author.identity})
         res.status(200).json(match)
     } catch (e) {
         res.status(400).json({error: e.message })
@@ -32,9 +34,10 @@ const updateMatch = async (req, res) => {
     const {id} = req.params
     const {home, away} = req.body
     if ( home && away ) req.body.fixture = home+' - '+away
+    let author = await Manager.findOne({_id:req.manager_id})
 
     try {
-        const match = await Match.findOneAndUpdate({_id: id}, {...req.body}, {new: true})
+        const match = await Match.findOneAndUpdate({_id: id}, {...req.body, author:author.identity}, {new: true})
         res.status(200).json(match)
     } catch (e) {
         res.status(400).json({error: "Match not found!"})
