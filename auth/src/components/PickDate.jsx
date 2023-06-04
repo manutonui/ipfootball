ff3bba8651857c3eb6cf48daec9e0f49e01e4a92
manutonui <tktonui@outlook.com>
2023-06-04 21:12:50 +0300

    added max of 7 days ahead in auth

--- auth/src/components/PickDate.jsx
+++ auth/src/components/PickDate.jsx
@@ -1,4 +1,5 @@
 const PickDate = ({date, handleDate}) => {
+    const maxDate = new Date().setDate(new Date().getDate() + 7)
     const handlePrev = () => {
         const prev = new Date(date)
         prev.setDate(date.getDate()-1)
@@ -14,7 +15,7 @@ const PickDate = ({date, handleDate}) => {
     return (
         <div className="pickDate">
             <button onClick={handlePrev} className="prev btn btn-sm btn-secondary">&laquo; Previous Date</button>
-            { (date < new Date()) && (<button onClick={handleNext} className="next btn btn-sm btn-secondary">Next Date &raquo;</button>) }
+            { (date < maxDate) && (<button onClick={handleNext} className="next btn btn-sm btn-secondary">Next Date &raquo;</button>) }
         </div>
     );
 }
